eec4e9b0439cf9669a2c965f80ef4c453de1ff7b
Muzaffar_Umarjonov <Muzaffar_Umarjonov@epam.com>
2023-03-13 10:22:04 +0500

    test case updated

--- src/test/java/com/epam/javamoduletask1/JavaModuleTask1ApplicationTests.java
+++ src/test/java/com/epam/javamoduletask1/JavaModuleTask1ApplicationTests.java
@@ -7,8 +7,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
